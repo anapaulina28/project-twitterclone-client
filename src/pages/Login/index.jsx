b0e84170bcfc36332511f489e0a30382e03d453a
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2023-08-31 17:34:57 +0100

    Thursday add

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,55 +1,65 @@
-import axios from 'axios'
-import React, { useContext, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { AuthContext } from '../../context/auth.context'
-const API_URL ='http://localhost:5005'
+import { useContext, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/auth.context";
 
-const Login = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [errorMessage, setErrorMessage] = useState(undefined)
+const API_URL = "http://localhost:5005";
 
-    const {storeToken, authenticateUser} = useContext(AuthContext)
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("undefined");
 
-    const navigate = useNavigate();
+  const {storeToken, authenticateUser}= useContext(AuthContext)
 
-    const fetchData = async () => {
-        try {
-            const requestBody = {email, password}
-            const response = await axios.post(`${API_URL}/auth/login`, requestBody)
-            storeToken(response.data.authToken)
-            authenticateUser();
-            navigate('/')
-        } catch (error) {
-            const errorDescription = error.response.data.message
-            setErrorMessage(errorDescription)
-        }
-       
-  
-    }
+  const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit= (e) =>{
+    e.preventDefault();
+ 
+    const requestBody = {email, password};
 
-        fetchData();
-    }
+    axios.post(`${API_URL}/auth/login`, requestBody)
+    .then((response)=>{
+        storeToken(response.data.authToken);
+        authenticateUser();
+        navigate('/')
 
-
+    }).catch((error)=>{
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription)
+    })
+  }
 
   return (
     <div>
-        <h1>Login</h1>
-        <form onSubmit={handleSubmit}>
-            <label>Email: <input type='email' name='email' value={email} onChange={(e)=> setEmail(e.target.value)}/></label>
-            <label>Password: <input type='password' name='password' value={password} onChange={(e)=> setPassword(e.target.value)}/></label>
-            <button type='submit'>Login</button>
-        </form>
-        {errorMessage && <p>{errorMessage}</p>}
-        <p>Don't have an account yet?</p>
-        <Link to='/signup'>Sign Up</Link>
-        
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+      {errorMessage && <p>{errorMessage}</p>}
+      <p>Do not have an account yet?</p>
+      <Link to="/signup">Sign Up</Link>
     </div>
-  )
+  );
 }
 
-export default Login
+export default LoginPage;
