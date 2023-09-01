2a5e2151a42d8e6acfc40fe85847015cb845e32c
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2023-09-01 16:51:40 +0100

    Change to HomePage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/auth.context';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const {isLoggedIn, user, logOutUser} = 
+    useContext(AuthContext);
+    return (
+      <nav>
+      <div>
+      <Link to="/">
+          <button>Home</button>
+      </Link>
+      </div>
+      {isLoggedIn? (
+        <div>
+          <button onClick={logOutUser}>Logout</button>
+          <p>{user && user.name}</p>
+        </div>
+      ): 
+      (
+        <div>
+          <Link to="/signup"><button>Signup</button></Link>
+          <Link to="/login"><button>Login</button></Link>
+        </div>
+      )
+    } 
+      </nav>
+    )
 }
 
 export default Home
